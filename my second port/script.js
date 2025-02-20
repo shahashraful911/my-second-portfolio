491968cbf44310c3fc1758deca66ad3bf1e9542d
shahashraful911 <shahash.911@gmail.com>
2025-02-20 11:17:12 -0800

    Add files via upload

---
+++ my second port/script.js
@@ -0,0 +1,11 @@
+function showGallery() {
+    alert("Gallery feature coming soon!");
+}
+
+function showFilms() {
+    alert("Short Films feature coming soon!");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Portfolio loaded successfully!");
+});
